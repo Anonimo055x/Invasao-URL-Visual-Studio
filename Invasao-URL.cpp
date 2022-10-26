c8f0cf2357d4c8021881d06e6126f40338c808d0
Matheus Maia <105084847+Anonimo055x@users.noreply.github.com>
2022-10-25 22:03:57 -0300

    Payload-Invasao-Auto-Executavel

---
+++ Invasao-URL.cpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <atlcomcli.h>
+#include <atldef.h>
+#include <Windows.h>
+#include <Ole2.h>
+#include <sdkddkver.h>
+#include <corecrt.h>
+#include <corecrt_memcpy_s.h>
+#include <vcruntime_string.h>
+#include <corecrt.h>
+#include <apiset.h>
+#include <apisetcconv.h>
+#include <minwindef.h>
+#include <minwinbase.h>
+
+class CodadoReply
+{
+	int main();
+
+#define CONNECT_COMPRESS_NETWORK_TRAFFIC ("active");
+#define CRT_BEGIN_C_HEADER = ("active");
+	#define MicrosoftCppCommontargets = ("active");
+};
+#define	IMPORT_OBJECT_CODE	url : [url da invasao]
+#define ActivationContextBasicInformation ('_APISETDEBUG_');
+#define reverse = ('192.168.1.100:443');
+#define credentials ('admin');
+#define directory ('C:/Users/mathe/Videos');
+
+int string();
+
+class MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+
+private:
+	#define OpenBackupEventLog ("define");
+
+};
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+#define open = ("define") = executar;
+} 
+
+
+	
